05d3cf5271db33e59ff40c8d5467ffbd6ed31a5c
Enri-Taar <enri654@hotmail.com>
2019-11-30 20:08:31 +0200

    ajamasin

--- skriptid/dota.js
+++ skriptid/dota.js
@@ -4,11 +4,17 @@ pilt.onclick = function muudaPilt(){
     //console.log(vanapilt)
     if (vanapilt.slice(-6) == "ey.jpg"){
         pilt.setAttribute("src", "images/puppey2018.jpg")
+        document.getElementById("ajamasin").innerHTML = "2018 aasta Puppey.";
     } else {
         vananumber = vanapilt.slice(-6,-4)
         if (vananumber > 10){
             vananumber--
             pilt.setAttribute("src", "images/puppey20"+vananumber+".jpg")
+            document.getElementById("ajamasin").innerHTML = '20'+vananumber+' aasta Puppey.';
+
+        }
+        else {
+            document.getElementById("ajamasin").innerHTML = 'Liiga kaugel minevikus!';
         }
     }
 
